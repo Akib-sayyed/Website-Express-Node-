1acb843e6be4c5bfa81002c8ec8bb737f90e2023
unknown <akiib.sayyed@gmail.com>
2020-05-29 23:34:43 +0530

    static pages added

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
+app.use(express.static(path.join(__dirname,"./static")));
+
 app.get("/",function(req,res){
-    res.send("hello");
+    res.sendFile(path.join(__dirname,"./static/index.html"));
+});
+
+app.get("/speakers",function(req,res){
+    res.sendFile(path.join(__dirname,"./static/speakers.html"));
 });
 
 app.listen(3000,function(){
