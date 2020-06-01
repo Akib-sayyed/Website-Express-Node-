c0bc89ed8f1c73da034bffc2836cb4e05ad3efcc
unknown <akiib.sayyed@gmail.com>
2020-06-01 20:30:53 +0530

    Routes and Middleware added

--- server.js
+++ server.js
@@ -1,21 +1,39 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
+
+const FeedbackService = require('./services/FeedbackService');
+const SpeakersService = require('./services/SpeakerService');
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakersService = new SpeakersService('./data/speakers.json');
+
+const routes = require('./routes');
 
 const app = express();
 
+const port = 3000;
+
+app.set('trust proxy', 1);
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['hello123', 'xyz'],
+  })
+);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static(path.join(__dirname, './static')));
 
-app.get('/', function (req, res) {
-  res.render('pages/index', { pageTitle: 'Welcome' });
-});
+app.use(
+  '/',
+  routes({
+    feedbackService,
+    speakersService,
+  })
+);
 
-app.get('/speakers', function (req, res) {
-  res.sendFile(path.join(__dirname, './static/speakers.html'));
-});
-
-app.listen(3000, function () {
-  console.log('Started');
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}!`);
 });
